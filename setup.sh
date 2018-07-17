a753546d59c4518adc0a7c22eed3ca995e72e6a4
WITwercode <41305452+WITwercode@users.noreply.github.com>
2018-07-17 15:18:09 -0500

    Match new_user.sh prompts

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@
 if [ -z "$1" ]; then
 	echo "Define a full name, can be pretty long (30 characters)"
 	echo "Does nothing but set a display name. Don't go crazy, though"
-	read -p: "Type the user's full name (e.g. John Wu): " fullname
+	read -p "Type the user's full name (e.g. John Wu): " fullname
 	echo "Create a username. No special characters or spaces alowed. Keep it short but unique."
-	read -p: "Type the username (e.g. wurules): " username
+	read -p "Type the username (e.g. wurules): " username
 	echo "Select a user ID. This is a number between 503 and 1000 and MUST BE UNIQUE."
 	echo "Be default, Mac makes the first account at 501 and the second at 503 then increments by 1/"
 	echo "You should do the same (first account is already taken, so start with 503."
