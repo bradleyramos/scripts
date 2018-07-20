f8d49997b3cb59c1950a6f28acb4a5f86e280a44
WITwercode <admin@WITWERADMN3.ads.northwestern.edu>
2018-07-20 10:30:31 -0500

    Echos manufactuerer when downloading

--- install_printer.sh
+++ install_printer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Usage: sudo sh install_printer.sh
 #You must run this as an admin (with sudo) or nothing will happen.
-#Installs a single printer whose model is already downloaded into witmac. Beware that model must 
+#Installs a single printer whose model is already downloaded into witmac. Beware that model must
 #exactly match the name of the driver. Will ask you for the admin password twice (lol).
 
 echo "Do you wish to see a list of valid models and their names?"
@@ -16,26 +16,26 @@ read -p "IP address of printer: " ipAdd
 read -r -p "Model of printer (name of driver, e.g. RICOH MP C6004): " model
 echo "Downloading driver, Input admin password"
 sftp admin@129.105.3.7:/Library/Printers/PPDs/Contents/Resources/\""$model"\" /Library/Printers/PPDs/Contents/Resources/\""$model"\"
-read -p "Manufacturer, Valid options: RICOH, Canon, KONICAMINOLTA, other (note case and spelling): " manu
+read -p "Manufacturer, Valid options: RICOH, Canon, KONICAMINOLTA, other " manu
 
 case "$manu" in
 "RICOH")
-	echo "Input admin password"
+	echo "Downloading $manu dependencies, Input admin password:"
 	scp -r  admin@129.105.3.7:/Library/Printers/RICOH /Library/Printers/Ricoh
 	;;
 "Canon")
-	echo "Input admin password"
+	echo "Downloading $manu dependencies, Input admin password:"
 	scp -r  admin@129.105.3.7:/Library/Printers/Canon /Library/Printers/Canon
 	;;
 "KONICAMINOLTA")
-	echo "Input admin password"
+	echo "Downloading $manu dependencies, Input admin password:"
 	scp -r  admin@129.105.3.7:/Library/Printers/KONICAMINOLTA /Library/Printers/KONICAMINOLTA
 	;;
 *)
-	echo "Skipping step"
+	echo "Not downloading manufacturer dependencies... hope it's an HP..."
 	;;
 esac
-	
+
 echo "Installing printer"
 /usr/sbin/lpadmin -p $ipAdd -o printer-is-shared="False" -E -v lpd://"$ipAdd" -P /Library/Printers/PPDs/Contents/Resources/"$model" -D \""$name"\"
 echo "It's over"
