50a4c8d70c1c605a6c40ddcd65d7092b41447203
Weinberg IT Administrator <admin@dhcp-10-105-114-236.wireless.northwestern.private>
2018-07-24 13:04:39 -0500

    Removed debug echo for repeat print add

--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@
 #so download the whole bloody thing.
 #Alternate usage: sudo sh setup.sh [fullname] [username] [uid] [password]
 
-RED='\033[0;31m'
-RESET='\033[0m'
-
-if [ -z "$1" ]; then
-	username="$(sh new_user.sh|tail -n 1)"
-else
-	fullname=$1
-	username=$2
-	uid=$3
-	changeme=$4
-
-	sh new_user.sh "$fullname" "$username" "$uid" "$changeme"
-fi
-
-echo ${RED}"User account created----------------------------------------"${RESET}
-sh no_lib_transfer.sh /Users/"$username" "$username"
-echo ${RED}"Data Transferred---------------------------------------------"${RESET}
-i=1
+# RED='\033[0;31m'
+# RESET='\033[0m'
+#
+# if [ -z "$1" ]; then
+# 	username="$(sh new_user.sh|tail -n 1)"
+# else
+# 	fullname=$1
+# 	username=$2
+# 	uid=$3
+# 	changeme=$4
+#
+# 	sh new_user.sh "$fullname" "$username" "$uid" "$changeme"
+# fi
+#
+# echo ${RED}"User account created----------------------------------------"${RESET}
+# sh no_lib_transfer.sh /Users/"$username" "$username"
+# echo ${RED}"Data Transferred---------------------------------------------"${RESET}
+# i=1
 
 
 # What's wrong here? who knows
