ad42a34f27eca2626f65bb3efbfca39a168ab879
Weinberg IT Administrator <admin@dhcp-10-105-114-236.wireless.northwestern.private>
2018-07-24 13:07:06 -0500

    Prompts to install first printer

--- setup.sh
+++ setup.sh
@@ -5,33 +5,32 @@
 #so download the whole bloody thing.
 #Alternate usage: sudo sh setup.sh [fullname] [username] [uid] [password]
 
-# RED='\033[0;31m'
-# RESET='\033[0m'
-#
-# if [ -z "$1" ]; then
-# 	username="$(sh new_user.sh|tail -n 1)"
-# else
-# 	fullname=$1
-# 	username=$2
-# 	uid=$3
-# 	changeme=$4
-#
-# 	sh new_user.sh "$fullname" "$username" "$uid" "$changeme"
-# fi
-#
-# echo ${RED}"User account created----------------------------------------"${RESET}
-# sh no_lib_transfer.sh /Users/"$username" "$username"
-# echo ${RED}"Data Transferred---------------------------------------------"${RESET}
-# i=1
+RED='\033[0;31m'
+RESET='\033[0m'
+
+if [ -z "$1" ]; then
+	username="$(sh new_user.sh|tail -n 1)"
+else
+	fullname=$1
+	username=$2
+	uid=$3
+	changeme=$4
+
+	sh new_user.sh "$fullname" "$username" "$uid" "$changeme"
+fi
+
+echo ${RED}"User account created----------------------------------------"${RESET}
+sh no_lib_transfer.sh /Users/"$username" "$username"
+echo ${RED}"Data Transferred---------------------------------------------"${RESET}
+i=1
 
 
-# What's wrong here? who knows
+# What's wrong here? who knows, but it works now (LOL)
 noyes='y'
 while [ "$noyes" = "y" ]; do
-	sh install_printer.sh
-	read -p "Add another printer? (y/n): " no
-	echo $no
+	read -p "Add a printer? (y/n): " no
 	if [ "$no" == "n" ]; then
 		break
 	fi
+	sh install_printer.sh
 done
