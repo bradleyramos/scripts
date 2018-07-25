901523204fe1b428492f57cb9fc3b3022b910f9d
Weinberg IT Administrator <admin@dhcp-10-105-114-236.wireless.northwestern.private>
2018-07-25 15:27:46 -0500

    Fixed checks of source dest conflict

--- no_lib_transfer.sh
+++ no_lib_transfer.sh
@@ -15,7 +15,6 @@ nos=1
 while [[ $nos == 1 ]]; do
   nos=0
   if [ -z "$3" ]; then
-    echo ""
     echo "Needs a full file path, such as /Volumes/Macintosh\ HD\ 1/Users/and314"
     echo "You must escape spaces: [\ ] instead of [ ]."
     echo "No Library files transferred except Firefox, Chrome, and Safari profiles"
@@ -30,13 +29,17 @@ while [[ $nos == 1 ]]; do
     destination=$1
   fi
 
-  while [ -z "$1" ] && [ -z "$3" ] && [ "$destination" == "$fileLoc"* ]; do
-    read -p "You're not very pro, are you? Press ctrl + c to cancel this process. Destination cannot be in Source." dummy
+  while [[ "$destination" == "$fileLoc"* ]]; do
+    if [ -z "$3" ]; then
+      nos=1
+      break
+    fi
+    read -p "Press ctrl + c to cancel this process. Destination cannot be in Source. " dummy
   done
 
   if [[ "$destination" == "$fileLoc"* ]]; then
-    read -p "$(echo "Are you sure you want to move files from within the source? \nThis will almostly certainly go very badly (y/n).")" yoes
-    if [["$yoes" == "y"]]; then
+    read -p "$(echo "Are you sure you want to move files from within the source? \nThis will almostly certainly go very badly (y/n): ")" yoes
+    if [[ "$yoes" == "y" ]]; then
       nos=0
     else
       nos=1
@@ -45,27 +48,27 @@ while [[ $nos == 1 ]]; do
 done
 
 
-
-mkdir -m777 -p "$destination"
-
-for dir in "$fileLoc/"*; do
-    if [[ "$dir" != *"/Library" ]]; then
-	     echo $dir
-	     cp -rp "$dir" "$destination"
-    fi
-done
-
-mkdir -m777 -p "$destination/Library"
-mkdir -m777 -p "$destination/Library/Application Support"
-
-cp -rp "$fileLoc/Library/Safari" "$destination/Library"
-cp -rp "$fileLoc/Library/Application Support/Google" "$destination/Library/Application Support"
-cp -rp "$fileLoc/Library/Application Support/Firefox" "$destination/Library/Application Support"
-
-if [ -z "$2" ]; then
-    read -p "Intended owner username (or Ctrl + C to skip): " fUser
-else
-    fUser=$2
-fi
-
-chown -R $fUser "$destination"
+#
+# mkdir -m777 -p "$destination"
+#
+# for dir in "$fileLoc/"*; do
+#     if [[ "$dir" != *"/Library" ]]; then
+# 	     echo $dir
+# 	     cp -rp "$dir" "$destination"
+#     fi
+# done
+#
+# mkdir -m777 -p "$destination/Library"
+# mkdir -m777 -p "$destination/Library/Application Support"
+#
+# cp -rp "$fileLoc/Library/Safari" "$destination/Library"
+# cp -rp "$fileLoc/Library/Application Support/Google" "$destination/Library/Application Support"
+# cp -rp "$fileLoc/Library/Application Support/Firefox" "$destination/Library/Application Support"
+#
+# if [ -z "$2" ]; then
+#     read -p "Intended owner username (or Ctrl + C to skip): " fUser
+# else
+#     fUser=$2
+# fi
+#
+# chown -R $fUser "$destination"
