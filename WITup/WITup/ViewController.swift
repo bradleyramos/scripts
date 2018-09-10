3ba6ea08f8fab37d8ff9bb80a2d19b338079ad7f
Bradley Ramos <bradleyramos@dhcp-10-105-227-17.wireless.northwestern.private>
2018-09-10 10:19:45 -0500

    Add WITup GUI-based setup

---
+++ WITup/WITup/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WITup
+//
+//  Created by Bradley Ramos on 9/7/18.
+//  Copyright © 2018 Weinberg IT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
