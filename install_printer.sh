cb8bbec677311948dfba5bbe9485e2637217949f
WuZJohn <admin@WITWERADMN3.ads.northwestern.edu>
2018-09-12 09:34:49 -0500

    Moved to single scp statement with repeat

--- install_printer.sh
+++ install_printer.sh
@@ -5,41 +5,53 @@
 #exactly match the name of the driver. Will ask you for the admin password twice (lol).
 RED='\033[0;31m'
 RESET='\033[0m'
+GREEN='\033[0;92m'
 
-echo "Do you wish to see a list of valid models and their names?"
-read -p "BEWARE, this list is long (y/n): " yesno
-if [ "$yesno" == "y" ]; then
-	echo "----------------------------------------------------------------------"
-	echo "Input admin password"
-	sh valid_drivers.sh
-fi
+echo "Downloading printer drivers, you will need the admin password below."
+scp admin@129.105.3.7:/Users/admin/printers.tar /Users/admin/printers.tar
+mkdir -m777 /Users/admin/PPDS
+tar -C "/Users/admin/PPDS" -xf "/Users/admin/printers.tar"
 
-read -p "Name the printer (no spaces): " name
-read -p "IP address of printer: " ipAdd
-#Why is there -r here? I have no idea
-read -r -p "Model of printer (name of driver, e.g. RICOH MP C6004): " model
-echo "Downloading driver, Input admin password"
-sftp admin@129.105.3.7:/Library/Printers/PPDs/Contents/Resources/\""$model"\" /Library/Printers/PPDs/Contents/Resources/\""$model"\"
-read -p "$(echo $RED"Manufacturer, Valid options: RICOH, Canon, KONICAMINOLTA, other: "$RESET)" manu
+restart=y
 
-case "$manu" in
-"RICOH")
-	echo "Downloading $manu dependencies, Input admin password:"
-	scp -r  admin@129.105.3.7:/Library/Printers/RICOH /Library/Printers/Ricoh
-	;;
-"Canon")
-	echo "Downloading $manu dependencies, Input admin password:"
-	scp -r  admin@129.105.3.7:/Library/Printers/Canon /Library/Printers/Canon
-	;;
-"KONICAMINOLTA")
-	echo "Downloading $manu dependencies, Input admin password:"
-	scp -r  admin@129.105.3.7:/Library/Printers/KONICAMINOLTA /Library/Printers/KONICAMINOLTA
-	;;
-*)
-	echo "Not downloading manufacturer dependencies... hope it's an HP..."
-	;;
-esac
+while [[ "$restart" == "y" ]]; then
+	echo "Do you wish to see a list of valid models and their names?"
+	read -p "BEWARE, this list is long (y/n): " yesno
+	if [ "$yesno" == "y" ]; then
+		echo "----------------------------------------------------------------------"
+		echo "Input admin password $GREEN"
+		ls "/Users/admin/PPDS/Printers/PPDs/Contents/Resources"
+		echo "$RESET"
+	fi
 
-echo $RED"Installing printer"$RESET
-/usr/sbin/lpadmin -p $ipAdd -o printer-is-shared="False" -E -v lpd://"$ipAdd" -P /Library/Printers/PPDs/Contents/Resources/"$model" -D "$name"
-echo "It's over"
+	read -p "Name the printer (no spaces): " name
+	read -p "IP address of printer: " ipAdd
+	#Why is there -r here? I have no idea
+	read -r -p "Model of printer (name of driver, e.g. RICOH MP C6004): " model
+	echo "Moving driver, Input admin password"
+	cp /Users/admin/PPDS/Printers/PPDs/Contents/Resources/\""$model"\" /Library/Printers/PPDs/Contents/Resources/\""$model"\"
+	read -p "$(echo $RED"Manufacturer, Valid options: RICOH, Canon, KONICAMINOLTA, other: "$RESET)" manu
+
+	case "$manu" in
+	"RICOH")
+		echo "Copying $manu dependencies, Input admin password:"
+		cp -r  /Users/admin/PPDS/Library/Printers/RICOH /Library/Printers/Ricoh
+		;;
+	"Canon")
+		echo "Copying $manu dependencies, Input admin password:"
+		cp -r  /Users/admin/PPDS/Library/Printers/Canon /Library/Printers/Canon
+		;;
+	"KONICAMINOLTA")
+		echo "Copying $manu dependencies, Input admin password:"
+		cp -r  /Users/admin/PPDS/Library/Printers/KONICAMINOLTA /Library/Printers/KONICAMINOLTA
+		;;
+	*)
+		echo "Not Copying manufacturer dependencies... hope it's an HP..."
+		;;
+	esac
+
+	echo $RED"Installing printer"$RESET
+	/usr/sbin/lpadmin -p $ipAdd -o printer-is-shared="False" -E -v lpd://"$ipAdd" -P /Library/Printers/PPDs/Contents/Resources/"$model" -D "$name"
+	echo "It's over"
+	read -p "Install another printer? (y/n): " restart
+done
--- valid_drivers.sh
+++ valid_drivers.sh
@@ -2,6 +2,7 @@
 #Usage: sh valid_drivers.sh
 #Shows you what drivers are available on witmac, but more importantly
 #provides an example of SSH usage.
+#Unused, now, in install_printer do to mass download format.
 
 ssh admin@129.105.3.7 "cd /Library/Printers/PPDs/Contents/Resources; ls"
 exit
