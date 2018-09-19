b47898737a4c7f7a95d0e78237b7a5ebf132c565
WuZJohn <admin@dhcp-129-105-3-236.wcas.northwestern.edu>
2018-09-19 16:42:42 -0500

    Allows for last names with spaces

--- simple_setup.sh
+++ simple_setup.sh
@@ -8,36 +8,40 @@ RED='\033[0;31m'
 RESET='\033[0m'
 GREEN='\033[0;92m'
 
-read -p "Type the Client's first name: " firstName
-read -p "Type the Client's last name: " lastName
-#Sends to lower case
-username="$(echo "$lastName" | awk '{print tolower($0)}')"
-
-echo "Concerning the Data transfer: "
-read -p "Is the old computer in Target mode and connected to this computer? (y/n): " noyes
-if [[ "$noyes" == "y" ]]; then
-    oldFile="/Volumes/Macintosh HD 1/Users"
-    type="user accounts"
-else
-    read -p "Is the data on an external hard drive? (y/n): " noyes
-    if [[ "$noyes" == "y" ]]; then
-        #In this case, we need to check the HFS+ Partition specifically (no users folder in this case)
-        oldFile="/Volumes/HFS+ Partition"
-        type="folders"
-    else
-        while [[ "1" == "1" ]]; do
-            #Must use not for dummies to customize source location
-            read -p "You're doomed. Use setup.sh instead. Press Ctrl + C to exit. " noyes
-        done
-    fi
-fi
+# read -p "Type the Client's first name: " firstName
+# read -p "Type the Client's last name: " lastName
+# #Sends to lower case
+# username="$(echo "$lastName" | awk '{print tolower($0)}')"
+# #Replaces spaces with underscores
+# username="${username// /_}"
+#
+#
+# echo "Concerning the Data transfer: "
+# read -p "Is the old computer in Target mode and connected to this computer? (y/n): " noyes
+# if [[ "$noyes" == "y" ]]; then
+#     oldFile="/Volumes/Macintosh HD 1/Users"
+#     type="user accounts"
+# else
+#     read -p "Is the data on an external hard drive? (y/n): " noyes
+#     if [[ "$noyes" == "y" ]]; then
+#         #In this case, we need to check the HFS+ Partition specifically (no users folder in this case)
+#         oldFile="/Volumes/HFS+ Partition"
+#         type="folders"
+#     else
+#         while [[ "1" == "1" ]]; do
+#             #Must use not for dummies to customize source location
+#             read -p "You're doomed. Use setup.sh instead. Press Ctrl + C to exit. " noyes
+#         done
+#     fi
+# fi
 
 #Next part is green to highlight part they need to read
 echo "$GREEN"
-ls "$oldFile"
+# ls "$oldFile"
+ls /Users/
 echo "$RESET"
 
-read -p "$(echo "${RED}Which of the above $type do you want to transfer?\n(Typically their netID, Lastname, or Firstname) $RESET"):" oldUser
+read -p "$(echo "${RED}Which of the above $type do you want to transfer?\n(Typically their netID, Last Name, or First Name) $RESET"):" oldUser
 fileLoc="$oldFile/$oldUser"
 
 #Copied from new_user.sh
