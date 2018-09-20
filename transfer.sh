195a2350dbf8d0e2ea8472a9959a6a52e7ec1e98
WuZJohn <admin@dhcp-129-105-3-236.wcas.northwestern.edu>
2018-09-20 13:21:26 -0500

    Typo

--- transfer.sh
+++ transfer.sh
@@ -28,7 +28,7 @@ while [[ $nos == 1 ]]; do
     destination=$1
   fi
 
-  while [[ "$destination" == "$fileLoc"* ]]; do
+  while [[ "$destination/"* == "$fileLoc" ]]; do
     if [ -z "$3" ]; then
       nos=1
       break
@@ -36,8 +36,8 @@ while [[ $nos == 1 ]]; do
     read -p "Press ctrl + c to cancel this process. Destination cannot be in Source. " dummy
   done
 
-  if [[ "$destination" == "$fileLoc"* ]]; then
-    read -p "$(echo "Are you sure you want to move files from within the source? \nThis will almostly certainly go very badly (y/n): ")" yoes
+  if [[ "$destination" == "$fileLoc/"* ]]; then
+    read -p "$(echo "Are you sure you want to move files to within the source? \nThis will almostly certainly go very badly (y/n): ")" yoes
     if [[ "$yoes" == "y" ]]; then
       nos=0
     else
@@ -52,7 +52,7 @@ read -p "Would you like to transfer Library files? (y/n): " yoes
 mkdir -m777 -p "$destination"
 
 for dir in "$fileLoc/"*; do
-    if [[ "$dir" != *"/Library" ] && [ "$dir" != *"/library_transfer" ]]; then
+    if [[ "$dir" != *"/Library" ]] && [[ "$dir" != *"/transfer_library" ]]; then
 	     echo $dir
 	     cp -rp "$dir" "$destination"
     fi
