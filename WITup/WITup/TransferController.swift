9aaa6e5ddfe15876497c9364f26d149313965b47
bradleyramos <39171556+bradleyramos@users.noreply.github.com>
2018-09-24 11:22:21 -0500

    Add quotes to path call

--- WITup/WITup/TransferController.swift
+++ WITup/WITup/TransferController.swift
@@ -95,7 +95,7 @@ class TransferController: NSViewController {
         
         // Run script
         var command = String()
-        command = "sudo" + " sh " + path + " " + firstName.stringValue + " " + lastName.stringValue + " " + sourceField.stringValue + " " + lib + " " + launch
+        command = "sudo" + " sh " + path + " " + firstName.stringValue + " " + lastName.stringValue + " \"" + sourceField.stringValue + "\" " + lib + " " + launch
         runLabel.stringValue = command
         //"Please remember to enable filevault permissions for new user"
         NSAppleScript(source: "do shell script \"" + command + "\" with administrator " +
