d3eebd333a3eba7e768e56008fa1c1d019414987
WuZJohn <admin@dhcp-129-105-3-236.wcas.northwestern.edu>
2018-10-03 15:22:00 -0500

    Prompt change

--- transfer.sh
+++ transfer.sh
@@ -48,6 +48,8 @@ done
 # Used later to create transfer_library file
 
 if [ -z "$4" ]; then
+  echo "You probably want to transfer Library files just in case."
+  echo "However, if this is the second transfer on the same dataset, use no."
   read -p "Would you like to transfer Library files? (y/n): " yoes
 else
   yoes=$4
