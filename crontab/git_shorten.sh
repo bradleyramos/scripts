1f792049de8de26a52fde78da0d98b4071559e2e
WuZJohn <admin@dhcp-10-105-160-224.wireless.northwestern.private>
2018-10-19 16:29:40 -0500

    git_shorten reduces time for download with scp

---
+++ crontab/git_shorten.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+cp -p /Users/admin/github/*.sh /Users/admin/smallhub
+cp -p /Users/admin/github/*.bat /Users/admin/smallhub
+cp -Rp /Users/admin/github/WITup\ app /Users/admin/smallhub
