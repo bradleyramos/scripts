7d9a53842ea3d936c17811884308d6f74c431335
WuZJohn <admin@dhcp-10-105-160-224.wireless.northwestern.private>
2018-10-19 16:40:10 -0500

    implement git_shorten

--- crontab/crontab.sh
+++ crontab/crontab.sh
@@ -1,3 +1,3 @@
 5 8 * * * cd /Users/admin && ./git_update.sh
 10 8 * * * cd /Users/admin && ./tar_printers.sh
-
+15 8 * * * cd /Users/admin && ./git_shorten.sh
--- crontab/git_update.sh
+++ crontab/git_update.sh
@@ -4,3 +4,4 @@ PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 git -C /Users/admin/github fetch
 git -C /Users/admin/github reset --hard
 git -C /Users/admin/github pull origin master
+chmod 711 /Users/admin/github/WITup\ app/WITup.app/Contents/MacOS/WITup
