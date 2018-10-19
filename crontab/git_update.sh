192d55bc0550e452c4fad2fe2f8536c26b80a0dc
WuZJohn <admin@dhcp-10-105-160-224.wireless.northwestern.private>
2018-10-19 16:36:53 -0500

    Reset --hard and permisions on WITup

--- crontab/git_update.sh
+++ crontab/git_update.sh
@@ -2,4 +2,5 @@
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 git -C /Users/admin/github fetch
+git -C /Users/admin/github reset --hard
 git -C /Users/admin/github pull origin master
